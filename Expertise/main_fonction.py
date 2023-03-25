b07e19a610c541563e6665b6d97c78b7021150ef
pouleetbasquaise <aion14killer@gmail.com>
2023-03-25 22:57:08 +0100

    new file:   Expertise/main_fonction.py

---
+++ Expertise/main_fonction.py
@@ -0,0 +1,26 @@
+#Les fonctions 
+
+'''nom = "tptp"
+
+print("je m'a^^elle" + nom) #concaténé la chaine
+print("je m'apelle toto")#Les deux sont pareil
+
+
+print("je m'appelle", nom, ...)# <-- 2 paramètres tu peux mettre X paramêtres avec une virgule. il concaténe chaque option + un espace : "Je m'appelle" + " " + nom + "j'ai" + "" 
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+nom1 = input("Nom de la personne 1: ")
+age1 = input ("Age de la personne 1: ")
+print("La personne 1 s'apelle", nom1, "elle a", age1)'''
+
+def afficher_infos_personne():
+    print("titi")
+    print("toto")
+    
+print("début du prog")
+
+afficher_infos_personne()
+
+print("fin prog")
